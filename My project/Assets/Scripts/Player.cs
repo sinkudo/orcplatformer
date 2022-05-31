c46285b18c3035f9cff6c0cb2561762e295d2a71
sinkudo <mishazagorodnikov@mail.ru>
2022-06-01 06:14:00 +0800

    тупо движение

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -4,16 +4,53 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+    int hp;
+    int mp;
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    [SerializeField] private bool isGrounded = false;
+    float moveHorizontal;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator animator;
+    private bool justJumped = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (!justJumped && Input.GetButtonDown("Jump") && isGrounded)
+            justJumped = true;
+    }
+    private void FixedUpdate()
+    {
+        checkGround();
+        if (moveHorizontal < -0.1f || moveHorizontal > 0.1f)
+            Run();
+        if (justJumped)
+            Jump();
         
+    }
+    void Run()
+    {
+        sprite.flipX = moveHorizontal < 0;
+        rb.AddForce(new Vector2(moveHorizontal * speed, 0f), ForceMode2D.Impulse);
+    }
+    void Jump()
+    {
+        justJumped = false;
+        rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+    }
+    void checkGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.1f);
+        isGrounded = colliders.Length > 1;
     }
 }

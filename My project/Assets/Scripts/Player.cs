ca91044b36c8b92f9fe359d6d32cf6c48b53b365
sinkudo <mishazagorodnikov@mail.ru>
2022-06-02 00:52:49 +0800

    Контр. прыжок

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -35,95 +35,34 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         checkGround();
-        //if (moveHorizontal < -0.1f || moveHorizontal > 0.1f)
-        //    Move();
         Move();
         Jump();
-        //if (justJumped)
-        //{
-        //    if (Input.GetButton("Jump"))
-        //        holdingJump = true;
-        //    else
-        //        holdingJump = false;
-        //    Jump();
-        //}
     }
     public  void Move()
     {
-        //print(transform.position);
-        //if(moveHorizontal == 0f)
-        //{
-        //    rb.velocity = Vector2.zero;
-        //    return;
-        //}
         if (moveHorizontal == -1f)
             sprite.flipX = true;
         else if (moveHorizontal == 1f)
             sprite.flipX = false;
 
-        //rb.AddForce(new Vector2(moveHorizontal * speed, 0f), ForceMode2D.Impulse);
         rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
-
-        //if (moveHorizontal == -1f)
-        //    sprite.flipX = true;
-        //else if (moveHorizontal == 1f)
-        //    sprite.flipX = false;
-        //rb.velocity = new Vector2(moveHorizontal * speed * Time.deltaTime, rb.velocity.y);
     }
-
-    //public bool jumpcontrol = false;
-    //public int jumpIter = 20;
-    //public int jumpValueIter = 60;
-    //public void Jump()
-    //{
-    //    print(jumpIter + " " + holdingJump);
-    //    if (jumpIter == 20)
-    //    {
-    //        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-    //        jumpIter++;
-    //    }
-    //    else if(jumpIter <= 30 && holdingJump)
-    //    {
-
-    //        rb.AddForce(Vector2.up * (jumpForce + jumpIter));
-    //        jumpIter++;
-    //    }
-    //    else
-    //    {
-    //        justJumped = false;
-    //        jumpIter = 20;
-    //    }
-    //}
 
     public bool jumpcontrol = false;
     public int jumpIter = 0;
-    public int jumpMax = 10;
-    //public void Jump()
-    //{
-    //    print(jumpIter + " " + holdingJump + " " + jumpMax);
-    //    if (jumpIter == 0 && holdingJump)
-    //    {
-    //        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-    //        jumpIter++;
-    //    }
-    //    else if (jumpIter <= jumpMax && holdingJump)
-    //    {
-    //        rb.AddForce(Vector2.up * jumpForce);
-    //        jumpIter++;
-    //    }
-    //    else
-    //    {
-    //        justJumped = false;
-    //        jumpIter = 0;
-    //    }
-    //}
-
+    [SerializeField] public int jumpMax = 10;
     public void Jump()
     {
         if (justJumped)
         {
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            justJumped = false;
+
+            if (Input.GetButton("Jump") && jumpIter++ < jumpMax)
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            else
+            {
+                jumpIter = 0;
+                justJumped = false;
+            }
         }
     }
 

db090e98533d6012647a3051864dc16c0e78773e
sinkudo <mishazagorodnikov@mail.ru>
2022-06-03 07:30:21 +0800

    кам

---
+++ My project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float camSpeed = 2f;
+    [SerializeField] private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!target) target = FindObjectOfType<Player>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = target.position;
+        pos.z = -10f;
+        transform.position = Vector3.Lerp(transform.position, pos, camSpeed * Time.deltaTime);
+    }
+}
